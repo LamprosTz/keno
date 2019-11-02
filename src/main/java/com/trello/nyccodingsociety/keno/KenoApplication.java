06ff1d790b0b2a7b417ee371cec7b04e5a8358f0
Anthony <maniatis.anthony@gmail.com>
2019-11-02 19:52:55 +0200

    Initial commit. Just a basic Spring Boot project that we'll keep building on.

---
+++ src/main/java/com/trello/nyccodingsociety/keno/KenoApplication.java
@@ -0,0 +1,13 @@
+package com.trello.nyccodingsociety.keno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenoApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(KenoApplication.class, args);
+	}
+}
