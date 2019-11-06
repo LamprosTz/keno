81d170bded367f532eef714ffe0d7e609637336e
LamprosTz <56821042+LamprosTz@users.noreply.github.com>
2019-11-06 11:00:50 +0200

    Add files via upload

---
+++ KenoApplication.java
@@ -0,0 +1,15 @@
+package com.trello.nyccodingsociety.keno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenoApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(KenoApplication.class, args);
+	}
+
+        System.out.println("Happy first day to work");
+}
